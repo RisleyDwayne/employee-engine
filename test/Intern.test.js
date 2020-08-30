6cc9b9b996f3f597d13e623594308521898f7a6b
Dwayne Risley <risleydwayne@gmail.com>
2020-08-30 13:16:08 -0400

    created Intern class

--- test/Intern.test.js
+++ test/Intern.test.js
@@ -3,13 +3,13 @@ const Intern = require("../lib/Intern");
 test("Can set school via constructor", () => {
   const testValue = "UCLA";
   const e = new Intern("Foo", 1, "test@test.com", testValue);
-  expect(e.school).toBe(testValue);
+  expect(e._school).toBe(testValue);
 });
 
 test("getRole() should return \"Intern\"", () => {
   const testValue = "Intern";
   const e = new Intern("Foo", 1, "test@test.com", "UCLA");
-  expect(e.getRole()).toBe(testValue);
+  expect(e.getRole()).toBe(testValue); 
 });
 
 test("Can get school via getSchool()", () => {
